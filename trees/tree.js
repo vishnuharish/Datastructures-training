a53483072c811c7c39d4f0ee3e9ee8274b0d84b2
Harish Gopalakrishnan <vishnuharish12@gmail.com>
2023-12-09 19:39:07 +0530

    Tree constructor and the insert method

--- trees/tree.js
+++ trees/tree.js
@@ -10,4 +10,28 @@ class BST {
   constructor(){
     this.root = null;
   }
+
+  insert(value){
+    const newNode = new Node(value);
+    if(!this.root) {
+      this.root = newNode;
+    }
+    let temp = this.root;
+    while(true){
+      if(newNode.value === temp.value) return undefined;
+      if(newNode.value < temp.value){
+        if(temp.left === null){
+          temp.left = newNode;
+          return this;
+        }
+        temp = temp.left;
+      } else {
+        if(temp.right === null){
+          temp.right = newNode;
+          return this;
+        }
+        temp = temp.right;
+      }
+    }
+  }
 }
